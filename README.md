cc1bb468d5195a2cd0fbbb5b61abb3d96e7c1cfa
lichenyi <lichenyi.1307@bytedance.com>
2022-04-27 20:58:20 +0800

    update

---
+++ README.md
@@ -0,0 +1,11 @@
+# 先执行npm link
+
+# 添加链接
+> 执行 gore --save "https://github.com/search?q={github}"  
+> 或: gore -s "https://github.com/search?q={github}"
+
+# 打开链接
+> 执行gore github gore
+>> 上放会用默认浏览器自动打开 https://github.com/search?q=gore
+
+#### 开始网上冲浪吧
---
+++ index.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+const shell = require('shelljs');
+// start tip
+const cmdTip = require('./assets/echoTip.json');
+const control = require('./lib/control');
+const tip = Object.keys(cmdTip);
+const tipStr = cmdTip[tip[Math.floor(Math.random() * tip.length)]];
+shell.echo(tipStr);
+// The above is a tip, which can be annotated
+control();
