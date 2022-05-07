4c0a93e7cb476307df4b547a20e084777b2d2cd3
lichenyi <lichenyi.1307@bytedance.com>
2022-05-07 18:34:54 +0800

    添加获取路径功能

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const shell = require('shelljs');
 // start tip
 const cmdTip = require('./assets/echoTip.json');
-const control = require('./lib/control');
+const control = require('./lib');
 const tip = Object.keys(cmdTip);
 const tipStr = cmdTip[tip[Math.floor(Math.random() * tip.length)]];
 shell.echo(tipStr);
